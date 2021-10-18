e7fbdc4da7169f03baf2965675a144b44cc6a423
Pedro Carvalho <pedropaulohouston@gmail.com>
2021-10-18 09:55:33 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sbyte x = 100;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 21474836478L; // o sufixo 'L'deixa explicito que estamos trabalhando com Long
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
