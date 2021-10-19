a38ef473d6ff1fc0674081f090647c712919c259
Pedro Carvalho <pedropaulohouston@gmail.com>
2021-10-19 08:42:03 -0300

    Day 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,16 +7,30 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            // sbyte x = 100;
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 21474836478L; // o sufixo 'L'deixa explicito que estamos trabalhando com Long
 
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
+            string produto1 = "Computador";
+            string produto2 = "Mesa de Escritorio";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine($"{produto1} cujo preco é ${preco1:f2}");
+            Console.WriteLine($"{produto2} cujo preco é ${preco2:f2}");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero {genero}");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Medida com oito casas decimais: {medida:f2}");
+            Console.WriteLine("Arredondado (três casas decimais): {0}", medida.ToString("F3"));
+            Console.WriteLine("Separador decimal invariant culture: " + medida.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
